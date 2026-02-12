69f94df60d3acd928a344a1608890a0901d31cbd
ardadizer <ardadizer17@gmail.com>
2026-02-13 00:13:31 +0300

    Fix full_name edge cases with strip

---
+++ hello.py
@@ -0,0 +1,23 @@
+# pylint: disable=missing-docstring
+
+import sys
+
+def full_name(first_name, last_name):
+    """returns the full name"""
+    first = first_name.capitalize() if first_name else ""
+    last = last_name.capitalize() if last_name else ""
+
+    return (first + " " + last).strip()
+
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        # => ['hello.py']
+        print(f'Hello{full_name("", "")}!')
+    elif len(sys.argv) == 2:
+        # => ['hello.py', 'John' ]
+        print(f'Hello {full_name(sys.argv[1], "")}!')
+    else:
+        # => ['hello.py', 'John', 'Lennon']
+        print(f"Hello {full_name(sys.argv[1], sys.argv[2])}!")
